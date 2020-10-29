c2c4ad4b89cee943de371052e2f59230021f9d4c
ClassmateZhang <zhangtongxueya@163.com>
2020-10-29 20:26:33 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+// 项目的启动入口
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式组件
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例
+// 把 router 配置到根实例
+// 通过 render 方法把 app 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
