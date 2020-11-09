8ded24128ec5f0fa2f9f01de59d0c99194a13fdf
ClassmateZhang <zhangtongxueya@163.com>
2020-11-09 21:34:34 +0800

    素材管理-收藏、删除样式完成

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -9,7 +9,7 @@
 
       </div>
       <div class="btn">
-        <el-radio-group v-model="collect" size="mini" @change="onCollectChange">
+        <el-radio-group v-model="collect" size="mini" @change="loadImages(1)">
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
@@ -17,24 +17,35 @@
         <el-button size="mini" type="success" @click="dialogUploadVisible = true">上传素材</el-button>
       </div>
       <el-row :gutter="30">
-        <el-col v-for="(item, id) in images" :key="id" :xs="12" :sm="6" :md="6" :lg="4" class="list">
+        <el-col v-for="(item, id) in images" :key="id" :xs="12" :sm="6" :md="6" :lg="4" class="image-item">
           <el-image style="height: 100px" :src="item.url" fit="cover"></el-image>
+          <!-- 删除和收藏 -->
+          <div class="image-active">
+            <!-- 收藏 -->
+            <i class="el-icon-star-on"></i>
+            <!-- 删除 -->
+            <i class="el-icon-delete-solid"></i>
+          </div>
         </el-col>
       </el-row>
-      <!-- 上传文件的对话框 -->
-      <el-dialog title="上传素材" :visible.sync="dialogUploadVisible" :append-to-body="true">
-        <!--
+    </el-card>
+    <!-- 分页 -->
+    <el-pagination background layout="prev, pager, next" :total="totalCount" :page-size="pageSize" :current-page="page"
+      @current-change="onPageChange">
+    </el-pagination>
+    <!-- 上传文件的对话框 -->
+    <el-dialog title="上传素材" :visible.sync="dialogUploadVisible" :append-to-body="true">
+      <!--
            用 upload 组件： 本身就支持请求-提交上传操作，使用它不需要自己去发请求，他自己会发;
            1 默认是:POST  2.请求路径:action: 完成路径   3. 请求头:
          -->
-        <el-upload class="upload-demo" drag action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
-          :headers="uploadHeaders" :on-success="onUploadSuccess" name="image" multiple>
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-      </el-dialog>
-    </el-card>
+      <el-upload class="upload-demo" drag action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+        :headers="uploadHeaders" :on-success="onUploadSuccess" name="image" multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -53,35 +64,51 @@ export default {
       collect: false, // 默认查询全部的素材
       images: [], // 图片的数据
       dialogUploadVisible: false, // 上传素材对话框,默认不显示
-      uploadHeaders: {
+      uploadHeaders: { // 上传素材的请求头设置
         Authorization: `Bearer ${user.token}`
-      }
+      },
+      page: 1, // 当前页
+      pageSize: 12, // 每页显示的条数
+      totalCount: 0 // 总页数
     }
   },
   created () {
     // 获取素材列表
-    this.loadImages(false)
+    this.loadImages(1)
   },
   methods: {
     // 获取素材列表
-    loadImages (collect = false) {
+    loadImages (page) {
+      this.page = page
       getImages({
-        collect // false 代表全部, true 代表收藏
+        collect: this.collect, // false 代表全部, true 代表收藏
+        page, // 当前页
+        per_page: this.pageSize // 每页的条数
       }).then(res => {
-        // console.log(res);
-        this.images = res.data.data.results
+        console.log(res)
+        this.images = res.data.data.results // 获取出来的素材
+        this.totalCount = res.data.data.total_count // 获取出来的条数
       })
     },
     // 全部和收藏按钮发送改变时
-    onCollectChange (value) {
-      console.log(value)
-      this.loadImages(value)
-    },
+    // onCollectChange() {
+    //   // console.log(this.collect)
+    //   this.loadImages(1) // 获取素材列表
+    // },
     onUploadSuccess () {
       // 关闭对话框
       this.dialogUploadVisible = false
       // 更新全部素材
-      this.loadImages(false)
+      this.loadImages() // 获取素材列表
+      this.$message({
+        type: 'success',
+        message: '上传成功'
+      })
+    },
+    // 点击页码的时候触发 点击了第几页
+    onPageChange (page) {
+      // console.log(page);
+      this.loadImages(page)
     }
   }
 }
@@ -96,8 +123,23 @@ export default {
   }
 
   // 每个图片的显示项
-  .list {
+  .image-item {
     margin-bottom: 30px;
+    position: relative;
+
+    // 图片的收藏和删除项
+    .image-active {
+      height: 35px;
+      position: absolute;
+      bottom: 4px;
+      left: 15px;
+      right: 15px;
+      background-color: rgba(204, 204, 204, 0.4);
+      font-size: 23px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
   }
 
 </style>
